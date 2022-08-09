47e20bb7333e80f1fe335557363d58434b81a34f
kkk125504 <kginyc5504@gmail.com>
2022-08-09 20:49:34 +0900

    게시물 리스팅시 데이터를 DB에서 가져오기 구현

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -3,6 +3,7 @@ package com.KoreaIT.example.JAM;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,9 +13,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
 		Scanner sc = new Scanner(System.in);
-
-		List<Article> articles = new ArrayList<>();
-		int lastId = 0;
+	
 		while (true) {
 			System.out.printf("명령어 ) ");
 			String cmd = sc.nextLine().trim();
@@ -78,6 +77,63 @@ public class Main {
 			} else if (cmd.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");				
 				
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+				ResultSet result = null;
+				List<Article> articles = new ArrayList<>();
+				
+				try {					
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/article_manager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "SELECT * FROM article";
+					sql += " ORDER BY id DESC;";
+					
+					System.out.println(sql);
+					pstmt = conn.prepareStatement(sql);
+					result = pstmt.executeQuery();
+					
+					while(result.next()) {
+						int id = result.getInt("id");
+						String regDate = result.getString("regDate");
+						String updateDate = result.getString("updateDate");
+						String title = result.getString("title");
+						String body = result.getString("body");
+			
+						Article article = new Article(id,regDate,updateDate,title,body);
+						
+						articles.add(article);
+						}										
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (result != null && !result.isClosed()) {
+							result.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 				if (articles.size() == 0) {
 					System.out.println("게시물이 없습니다.");
 					continue;
