968124163fa74d72f576450ca1614554d1e6cd93
kkk125504 <kginyc5504@gmail.com>
2022-08-11 23:52:37 +0900

    게시물 삭제 기능 구현

--- src/com/KoreaIT/example/JAM/App.java
+++ src/com/KoreaIT/example/JAM/App.java
@@ -77,7 +77,33 @@ public class App {
 			sql.append(", title = ?", title);
 			sql.append(", `body` = ?", body);
 
-			DBUtil.insert(conn, sql);
+			int id = DBUtil.insert(conn, sql);
+
+			System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
+
+		} else if (cmd.startsWith("article delete ")) {
+			int id = Integer.parseInt(cmd.split(" ")[2]);
+	
+			SecSql sql = new SecSql();
+			sql.append("SELECT COUNT(*) FROM article");			
+			sql.append(" WHERE id = ?", id);
+			
+			int articlesCount = DBUtil.selectRowIntValue(conn, sql);
+
+			if (articlesCount == 0) {
+				System.out.printf("%d번 게시글은 존재하지 않습니다.\n", id);
+				return 0;
+			}
+			
+			System.out.printf("== %d번 게시물 삭제 ==\n", id);
+			
+			sql = new SecSql();
+			sql.append("DELETE FROM article");			
+			sql.append(" WHERE id = ?", id);
+
+			DBUtil.delete(conn, sql);
+
+			System.out.printf("%d번 게시물이 삭제 되었습니다\n", id);
 
 		} else if (cmd.startsWith("article modify ")) {
 			int id = Integer.parseInt(cmd.split(" ")[2]);
@@ -103,7 +129,7 @@ public class App {
 		} else if (cmd.equals("article list")) {
 
 			System.out.println("== 게시물 리스트 ==");
-		
+
 			List<Article> articles = new ArrayList<>();
 
 			SecSql sql = new SecSql();
@@ -112,8 +138,8 @@ public class App {
 			sql.append(" ORDER BY id DESC");
 
 			List<Map<String, Object>> articlesListMap = DBUtil.selectRows(conn, sql);
-			
-			for(Map<String, Object> articleMap : articlesListMap) {
+
+			for (Map<String, Object> articleMap : articlesListMap) {
 				articles.add(new Article(articleMap));
 			}
 
@@ -129,7 +155,6 @@ public class App {
 			}
 
 		}
-
 		if (cmd.equals("exit")) {
 			System.out.println("프로그램을 종료합니다");
 			return -1;
